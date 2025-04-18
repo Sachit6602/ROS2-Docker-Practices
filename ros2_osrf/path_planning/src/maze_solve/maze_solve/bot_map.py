02356862659ce9becef6c1f41689da37fc1e10c8
Sachit Ravikumar <79889525+Sachit6602@users.noreply.github.com>
2025-04-18 04:36:50 +0100

    Update bot_map.py

--- ros2_osrf/path_planning/src/maze_solve/maze_solve/bot_map.py
+++ ros2_osrf/path_planning/src/maze_solve/maze_solve/bot_map.py
@@ -1,7 +1,30 @@
 import cv2
 import numpy as np
 
-draw_intrstpts = False
+draw_intrstpts = True
+debug_mapping = False
+
+class Graph():
+    def __init__(self):
+        self.graph = {}
+
+        self.start = 0
+        self.end = 0
+    
+    def add_vertex(self,vertex, neighbor = None, case =  None, cost = None):
+        if vertex in self.graph.keys():
+            self.graph[vertex][neighbor] = {}
+            self.graph[vertex][neighbor]["case"] = case
+            self.graph[vertex][neighbor]["cost"] =  cost
+
+        else:
+            self.graph[vertex] = {}
+            self.graph[vertex]["case"] = case
+
+    def displaygraph(self):
+        for key,value in self.graph.items():
+            print("key {} has value {} ". format(key,value))
+
 
 class bot_mapper():
 
@@ -12,7 +35,145 @@ class bot_mapper():
 
         # Cropping control for removing maze boundary
         self.crp_amt = 5
+
+        self.Graph = Graph()
+
+        # State variables to define the connection status of each vertex
+        self.connected_left = False
+        self.connected_upleft = False
+        self.connected_up = False
+        self.connected_upright = False
+
+        # Maze (Colored) for displaying connection between nodes
+        self.maze_connect = []
+
+        self.maze = 0
+
+    # Display connection between nodes with a colored line
+    def display_connected_nodes(self,curr_node,neighbor_node,case="Unkown",color=(0,0,255)):
+        curr_pixel = (curr_node[1],curr_node[0])
+        neighbor_pixel = (neighbor_node[1],neighbor_node[0])
+        #self.maze_connect= cv2.circle(self.maze_connect, curr_pixel, 5, (255,0,0))
+        #self.maze_connect= cv2.circle(self.maze_connect, neighbor_pixel, 5, (255,0,0))
+        print("----------------------) CONNECTED >> {} << ".format(case))
+        self.maze_connect = cv2.line(self.maze_connect,curr_pixel,neighbor_pixel,color,1)
+        cv2.imshow("Nodes Conected", self.maze_connect)
+        if debug_mapping:
+            cv2.waitKey(0)                    
+            self.maze_connect = cv2.line(self.maze_connect,curr_pixel,neighbor_pixel,(255,255,255),1)
+
+    def connect_neighbors(self,maze,node_row,node_col,case,step_l = 1,step_up = 0,totl_cnncted = 0):
+        curr_node = (node_row,node_col)
+
+        # Check if there is a path around our node        
+        if (maze[node_row-step_up][node_col-step_l]>0):
+            # There is a path ==> Look for neighbor node to connect
+            neighbor_node = (node_row-step_up,node_col-step_l)
+            # If potential_neighbor_node is actually a key in graph                
+            if neighbor_node in self.Graph.graph.keys():
+            
+                neighbor_case = self.Graph.graph[neighbor_node]["case"]
+                cost = max(abs(step_l),abs(step_up))
+                totl_cnncted +=1
+
+                self.Graph.add_vertex(curr_node,neighbor_node,neighbor_case,cost)
+                self.Graph.add_vertex(neighbor_node,curr_node,case,cost)
+                print("\nConnected {} to {} with Case [step_l,step_up] = [ {} , {} ] & Cost -> {}".format(curr_node,neighbor_node,step_l,step_up,cost))
+
+                if not self.connected_left:
+                    self.display_connected_nodes(curr_node, neighbor_node,"LEFT",(0,0,255))
+                    # Vertex has connected to its left neighbor.                    
+                    self.connected_left = True
+                    # Check up-Left route now
+                    step_l = 1
+                    step_up = 1
+                    self.connect_neighbors(maze, node_row, node_col, case,step_l,step_up,totl_cnncted)
+                if not self.connected_upleft:
+                    self.display_connected_nodes(curr_node, neighbor_node,"UPLEFT",(0,128,255))
+                    # Vertex has connected to its up-left neighbor.
+                    self.connected_upleft = True
+                    # Check top route now
+                    step_l  = 0
+                    step_up = 1
+                    self.connect_neighbors(maze, node_row, node_col, case,step_l,step_up,totl_cnncted)
+                if not self.connected_up:
+                    self.display_connected_nodes(curr_node, neighbor_node,"UP",(0,255,0))
+                    # Vertex has connected to its up neighbor.
+                    self.connected_up = True
+                    # Check top-right route now
+                    step_l  = -1
+                    step_up = 1
+                    self.connect_neighbors(maze, node_row, node_col, case,step_l,step_up,totl_cnncted)
+                if not self.connected_upright:
+                    self.display_connected_nodes(curr_node, neighbor_node,"UPRIGHT",(255,0,0))
+                    # Vertex has connected to its up-right neighbor.
+                    self.connected_upright = True
+
+            # Still searching for node to connect in a respective direction
+            if not self.connected_upright:
+                if not self.connected_left:
+                    # Look a little more left, You'll find it ;)
+                    step_l +=1
+                elif not self.connected_upleft:
+                    # Look a little more (diagnolly) upleft, You'll find it ;)
+                    step_l+=1
+                    step_up+=1
+                elif not self.connected_up:
+                    # Look a little more up, You'll find it ;)
+                    step_up+=1
+                elif not self.connected_upright:
+                    # Look a little more upright, You'll find it ;)
+                    step_l-=1
+                    step_up+=1
+                self.connect_neighbors(maze, node_row, node_col, case,step_l,step_up,totl_cnncted)
+
+        else:
+            # No path in the direction you are looking, Cycle to next direction
+            if not self.connected_left:
+                # Basically there is a wall on left so just start looking up lft:)
+                self.connected_left = True
+                # Looking upleft now
+                step_l = 1
+                step_up = 1
+                self.connect_neighbors(maze, node_row, node_col, case,step_l,step_up,totl_cnncted)
+            elif not self.connected_upleft:
+                # Basically there is a wall up lft so just start looking up :)
+                self.connected_upleft = True
+                step_l = 0
+                step_up = 1
+                self.connect_neighbors(maze, node_row, node_col, case, step_l, step_up, totl_cnncted)
+                
+
+            elif not self.connected_up:
+                # Basically there is a wall above so just start looking up-right :)
+                self.connected_up = True
+                step_l = -1
+                step_up = 1
+                self.connect_neighbors(maze, node_row, node_col, case, step_l, step_up, totl_cnncted)
+
+            elif not self.connected_upright:
+                # Basically there is a wall above so just start looking up-right :)
+                self.connected_upright = True
+                step_l = 0
+                step_up = 0                
+                return  
+            
+
+    # function to draw a triangle around a point    
+    @staticmethod
+    def triangle(image,ctr_pt,radius,colour=(0,255,255),thickness=2):
+        # Polygon corner points coordinates
+        pts = np.array( [ [ctr_pt[0]        , ctr_pt[1]-radius]  , 
+                          [ctr_pt[0]-radius , ctr_pt[1]+radius]  ,
+                          [ctr_pt[0]+radius , ctr_pt[1]+radius]   
+                        ] 
+                        ,np.int32
+                      )
         
+        pts = pts.reshape((-1, 1, 2))
+        
+        image = cv2.polylines(image,[pts],True,colour,thickness)
+        return image
 
     @staticmethod
     def get_surround_pixel_intensities(maze, curr_row, curr_col):
@@ -92,8 +253,27 @@ class bot_mapper():
 
         return top_left,top,top_rgt,rgt,btm_rgt,btm,btm_left,lft,no_of_pathways
 
+    def reset_connect_parameters(self):
+        # Reseting member variables to False initially when looking for nodes to connect
+        self.connected_left = False
+        self.connected_upleft = False
+        self.connected_up = False
+        self.connected_upright = False
 
     def one_pass(self,maze):
+
+        # Remove previously found nodes
+        self.Graph.graph.clear()
+
+        # Initalizing Maze_connect with Colored Maze
+        self.maze_connect = cv2.cvtColor(maze, cv2.COLOR_GRAY2BGR)
+        cv2.namedWindow("Nodes Conected",cv2.WINDOW_FREERATIO)
+
+        # Initialize counts of Ip's
+        turns = 0
+        junc_3 = 0
+        junc_4 = 0
+
         maze_bgr = cv2.cvtColor(maze, cv2.COLOR_GRAY2BGR)
 
         #Creating a window to display Detected Interest Points
@@ -103,7 +283,11 @@ class bot_mapper():
 
         for row in range(rows):
             for col in range(cols):
+
                 if (maze[row][col]==255):
+                    if debug_mapping:
+                        # Re-Initalizing Maze_connect with Colored Maze
+                        self.maze_connect = cv2.cvtColor(maze, cv2.COLOR_GRAY2BGR)
                     #Probabale Interest Point
                     top_left,top,top_rgt,rgt,btm_rgt,btm,btm_left,lft, paths = self.get_surround_pixel_intensities(maze.copy(),row,col)
 
@@ -112,11 +296,17 @@ class bot_mapper():
                             # Start
                             maze_bgr[row][col] = (0,128,255)
                             cv2.imshow("Maze (Interest Points)",maze_bgr)
-                        
+                            self.Graph.add_vertex((row, col),case= "_Start_")
+                            self.Graph.start = (row, col)
                         else:
                             # End (MAze Exit)
                             maze_bgr[row][col] = (0,255,0)
-                            cv2.imshow("Maze (Interest Points)",maze_bgr)  
+                            cv2.imshow("Maze (Interest Points)",maze_bgr) 
+                            self.Graph.add_vertex((row, col),case= "_End_")
+                            self.Graph.send = (row, col)
+                            self.reset_connect_parameters()
+
+                            self.connect_neighbors(maze, row, col, "_End_")
                     # Check if it is a (Dead End)        
                     elif (paths==1):
                         crop = maze[row-1:row+2,col-1:col+2]
@@ -125,6 +315,9 @@ class bot_mapper():
                         if draw_intrstpts:
                             maze_bgr= cv2.circle(maze_bgr, (col,row), 10, (0,0,255),4)                       
                         cv2.imshow("Maze (Interest Points)",maze_bgr)
+                        self.Graph.add_vertex((row,col),case = "_DeadEnd_")
+                        self.reset_connect_parameters()
+                        self.connect_neighbors(maze, row, col, "_End_")
                     # Check if it is either a *Turn* or just an ordinary path
                     elif (paths==2):
                         crop = maze[row-1:row+2,col-1:col+2]
@@ -134,10 +327,15 @@ class bot_mapper():
                         if not ( ( (2 - nzero_ptA[0])==nzero_ptB[0] ) and 
                                     ( (2 - nzero_ptA[1])==nzero_ptB[1] )     ):
                             maze_bgr[row][col] = (255,0,0)
-                            #if draw_intrstpts:
-                                #maze_bgr= cv2.circle(maze_bgr, (col,row), 10, (255,0,0),2)                            
+                            if draw_intrstpts:
+                                maze_bgr= cv2.circle(maze_bgr, (col,row), 10, (255,0,0),2)                            
                             cv2.imshow("Maze (Interest Points)",maze_bgr)
 
+                            self.Graph.add_vertex((row,col),case = "_Turn_")
+                            self.reset_connect_parameters()
+                            self.connect_neighbors(maze, row, col, "_Turn_")
+                            turns+=1                          
+                            
                      # Check if it is either a *3-Junc* or a *4-Junc*
                     elif (paths>2):
                         if (paths ==3):
@@ -147,13 +345,23 @@ class bot_mapper():
                             
                             cv2.imshow("Maze (Interest Points)",maze_bgr)
 
+                            self.Graph.add_vertex((row,col),case = "_3-Junc_")
+                            self.reset_connect_parameters()
+                            self.connect_neighbors(maze, row, col, "_3-Junc_")
+                            junc_3+=1 
+
                         else:
                             maze_bgr[row][col] = (128,0,128)
                             if draw_intrstpts:
                                 cv2.rectangle(maze_bgr,(col-10,row-10) , (col+10,row+10), (255,215,0),4)
                             cv2.imshow("Maze (Interest Points)",maze_bgr)
+
+                            self.Graph.add_vertex((row,col),case = "_4-Junc_")
+                            self.reset_connect_parameters()
+                            self.connect_neighbors(maze, row, col, "_4-Junc_")
+                            junc_4+=1 
                         
-                          
+        print("\nInterest Points !!! \n[ Turns , 3_Junc , 4_Junc ] [ ",turns," , ",junc_3," , ",junc_4," ] \n")                  
                         
 
 
@@ -190,7 +398,8 @@ class bot_mapper():
             
             # identify Interest Points in the path to further reduce processing time
             self.one_pass(thinned_cropped)
-            #cv2.waitKey(0)
+            cv2.waitKey(0)
+
             self.maze = thinned_cropped
-            self.graphified = True
+
 
